c7e42de6f212d1dfb0886eed907043359c52514f
Kelsey Low <kklowlow@gmail.com>
2019-05-11 01:53:29 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,22 +30,16 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
-      ####### bug fix
-      if !params[:owner].keys.include?("owner_id")
-      params[:owner]["owner_id"] = []
+      @pet = Pet.find(params[:id])
+      @pet.update(params["pet"])
+      if !params["owner"]["name"].empty?
+        @pet.owner << Owner.create(name: params["owner"]["name"])
       end
-      #######
-   
-      @owner = Owner.find(params[:id])
-      @owner.update(params["owner"])
-      if !params["pet"]["name"].empty?
-        @owner.pets << Pet.create(name: params["pet"]["name"])
-      end
-      redirect "owners/#{@owner.id}"
+      redirect to "pets/#{@pet.id}"
   end
 
   patch '/pets/:id' do 
 
-    redirect to "pets/#{@pet.id}"
+   
   end
 end
